12831458fdcb05986d918ae55123024c958caa9f
YuhIcey <cooperpro24@gmail.com>
2025-04-15 19:07:15 -0700

    ChatSystem.cpp

--- ChatSystem.cpp
+++ ChatSystem.cpp
@@ -1,8 +1,13 @@
 #include "ChatSystem.h"
 #include <iostream>
+#include <unordered_map>
+#include <string>
 
 namespace MadreChat
 {
+    // Storage for private messages
+    std::unordered_map<std::string, std::vector<std::string>> privateMessages;
+
     void Init()
     {
         std::cout << "[ChatSystem] Initialized\n";
@@ -11,5 +16,24 @@ namespace MadreChat
     void Shutdown()
     {
         std::cout << "[ChatSystem] Shutdown\n";
+    }
+
+    void SendMessage(const std::string& user, const std::string& message)
+    {
+        std::cout << user << ": " << message << "\n";
+    }
+
+    void SendPrivateMessage(const std::string& fromUser, const std::string& toUser, const std::string& message)
+    {
+        std::string formattedMessage = "[Private] " + fromUser + " -> " + toUser + ": " + message;
+        std::cout << formattedMessage << "\n";
+        privateMessages[toUser].push_back(formattedMessage);
+    }
+
+    void ListCommands()
+    {
+        std::cout << "Available Commands:\n";
+        std::cout << "/help - Show available commands\n";
+        std::cout << "/whisper <user> <message> - Send a private message\n";
     }
 }
